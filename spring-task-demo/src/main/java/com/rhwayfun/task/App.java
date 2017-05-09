d53dfac62c3476abfe073fd609a471037602348b
chance.zhao@Tianrong <18348370447@163.com>
2017-05-09 17:30:16 +0800

    spring定时器 非注解

---
+++ spring-task-demo/src/main/java/com/rhwayfun/task/App.java
@@ -0,0 +1,55 @@
+package com.rhwayfun.task;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class App {
+	
+	 	public void execute1(){
+	        System.out.printf("Task: %s, Current time: %s\n", 1, LocalDateTime.now());
+	    }
+
+	    public void execute2(){
+	        System.out.printf("Task: %s, Current time: %s\n", 2, LocalDateTime.now());
+	    }
+
+	    public void execute3(){
+	        System.out.printf("Task: %s, Current time: %s\n", 3, LocalDateTime.now());
+	    }
+
+	    public void execute4(){
+	        System.out.printf("Task: %s, Current time: %s\n", 4, LocalDateTime.now());
+	    }
+
+	    public void execute5(){
+	        System.out.printf("Task: %s, Current time: %s\n", 5, LocalDateTime.now());
+	    }
+
+	    public void execute6(){
+	        System.out.printf("Task: %s, Current time: %s\n", 6, LocalDateTime.now());
+	    }
+
+	    public void execute7(){
+	        System.out.printf("Task: %s, Current time: %s\n", 7, LocalDateTime.now());
+	    }
+
+	    public void execute8(){
+	        System.out.printf("Task: %s, Current time: %s\n", 8, LocalDateTime.now());
+	    }
+
+	    public void execute9(){
+	        System.out.printf("Task: %s, Current time: %s\n", 9, LocalDateTime.now());
+	    }
+
+	    public void execute10(){
+	        System.out.printf("Task: %s, Current time: %s\n", 10, LocalDateTime.now());
+	    }
+
+	    public void execute11(){
+	        System.out.printf("Task: %s, Current time: %s\n", 11, LocalDateTime.now());
+	    }
+
+}
