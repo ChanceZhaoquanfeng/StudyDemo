f6b4322b658be40aee4b110e7f912b4c8a912e40
chance.zhao@Tianrong <18348370447@163.com>
2017-05-11 10:49:30 +0800

    spring整合mq范例资料

---
+++ spring-mq-demo/src/main/java/com/chance/mq/consumer/queue/QueueReceiver2.java
@@ -0,0 +1,28 @@
+
+package com.chance.mq.consumer.queue;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author liang
+ * @description  队列消息监听器
+ * 
+ */
+@Component
+public class QueueReceiver2 implements MessageListener {
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("QueueReceiver2接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ spring-mq-demo/src/main/java/com/chance/mq/consumer/topic/TopicReceiver2.java
@@ -0,0 +1,28 @@
+package com.chance.mq.consumer.topic;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 
+ * @author liang
+ * @description  Topic消息监听器
+ * 
+ */
+@Component
+public class TopicReceiver2 implements MessageListener{
+
+
+	public void onMessage(Message message) {
+		try {
+			System.out.println("TopicReceiver2接收到消息:"+((TextMessage)message).getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
