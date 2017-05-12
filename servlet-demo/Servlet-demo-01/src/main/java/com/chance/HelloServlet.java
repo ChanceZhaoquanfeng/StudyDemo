07b6cfb0f73f86014ef742905f1486c0fe54b786
chance.zhao@Tianrong <18348370447@163.com>
2017-05-12 16:48:02 +0800

    servlet学习Demo

---
+++ servlet-demo/Servlet-demo-01/src/main/java/com/chance/HelloServlet.java
@@ -0,0 +1,43 @@
+package com.chance;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * servletDemo 
+ * 重写doget dopost方法
+ * @author chance
+ *
+ */
+public class HelloServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);// 调用doGet()
+	}
+	/**
+	 * HttpServletRequest 网页发过来的请求
+	 * HttpServletResponse 返回给网页的响应
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 设置输出格式，解决中文乱码问题
+		response.setContentType("text/html; charset=utf-8");
+		// 获取输出流对象
+		PrintWriter out = response.getWriter();
+		// 向浏览器输出内容
+		out.write("这是我的第一个Servlet代码");
+		out.flush();
+		out.close();
+	}
+
+}
